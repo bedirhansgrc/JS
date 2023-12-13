b2f10abed15f6248c6b3f431d8fdc7bb67949bc8
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-13 22:26:32 +0300

    Variable Types

---
+++ JavaScript Recap/Variable Types/script.js
@@ -0,0 +1,17 @@
+var urunAdi = "ipjone 13"; //string
+let urunFiyat = 15000; //number
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+
+let sayi1 = 10;
+let sayi2 = 20;
+
+console.log(sayi1.toString() + sayi2.toString())
+
+let sinavsonucu = 70;
+
+let basarilimi = (sinavsonucu > 50);
+
+console.log(basarilimi);
+console.log(typeof basarilimi);
