3472732844bd971783f7d75a1c153a6508aaeafc
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-14 19:39:59 +0300

    Create script.js

---
+++ JavaScript Recap/Operators/script.js
@@ -0,0 +1,25 @@
+let sonuc;
+let a = 10, b = 20, c = 30;
+
+// 1-Aritmetik Operatörler
+sonuc = a + b;
+sonuc = a - b;
+sonuc = a * b;
+sonuc = a / b;
+sonuc = a % b;
+
+// 2-Atama Operatörleri
+sonuc = a;
+sonuc += a; //sonuc = sonuc + a
+
+// 3-Karşılaştırma Operatörleri
+
+sonuc = (a == b);
+sonuc = (a != b);
+sonuc = (3 === "3") //değer tip
+sonuc = (a < b)
+sonuc = (a > b)
+// 4-Mantıksal Operatörler
+
+console.log(sonuc);
+
