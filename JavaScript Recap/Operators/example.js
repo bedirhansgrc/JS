58333aef97aff5701a3e5c54b34434b4e1524ab5
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-15 12:11:54 +0300

    Create example.js

---
+++ JavaScript Recap/Operators/example.js
@@ -0,0 +1,14 @@
+let sayi = 29;
+
+if (10 <= sayi <= 50) {
+    console.log("sayi 10 ve 50 arasındadır");
+} else {
+    console.log("sayı 10 ile 50 arasında değildir.");
+}
+
+if (0 < sayi && sayi % 2 == 1) {
+    console.log("sayi pozitif tek sayıdır")
+} else {
+    console.log("sayi pozitif tek sayı değildir");
+}
+
