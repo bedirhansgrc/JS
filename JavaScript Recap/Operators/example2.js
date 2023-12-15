75051705a66baa56ab3af59702b074e61b1218a9
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-15 12:29:31 +0300

    Update example2.js

--- JavaScript Recap/Operators/example2.js
+++ JavaScript Recap/Operators/example2.js
@@ -1,4 +1,4 @@
-let x = 10, y = 20, z = 0
+let x = 20, y = 20, z = 20
 
 if (x > y && x > z) {
     console.log("x en büyük")
@@ -6,4 +6,6 @@ if (x > y && x > z) {
     console.log("y en büyük")
 }else if (z > y && z > x){
     console.log("z en büyük")
+}else{
+    console.log("sayilar esittir")
 }
