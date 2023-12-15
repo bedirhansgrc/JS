569d2d7a7c1aef530d5d9197a432ac5f63272704
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-15 11:54:51 +0300

    Create script2.js

---
+++ JavaScript Recap/Operators/script2.js
@@ -0,0 +1,17 @@
+let username = "bedirhansigirci";
+let password = "1234"
+
+if (username == "bedirhansigirci") {
+    if (password == "1234") {
+        console.log("uygulamaya giriş yapıldı");
+    } else{
+        console.log("parola yanlış")
+    }
+}
+ else {
+     console.log("kullanıcı adı yanlış")
+ }
+
+// if (!isLoggedin) {
+//     console.log("uygulamaya giriş yapılmadı")
+// }
