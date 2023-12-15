7b61103ebf918d37bcbf9d09080b8ee14da1a708
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-15 12:28:38 +0300

    Create example2.js

---
+++ JavaScript Recap/Operators/example2.js
@@ -0,0 +1,9 @@
+let x = 10, y = 20, z = 0
+
+if (x > y && x > z) {
+    console.log("x en büyük")
+}else if(y > x && y > z) {
+    console.log("y en büyük")
+}else if (z > y && z > x){
+    console.log("z en büyük")
+}
