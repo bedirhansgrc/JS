e039d4e070ea2d17ab141263415d125912c21feb
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-16 19:07:26 +0300

    Create examples.js

---
+++ JavaScript Recap/Strings/examples.js
@@ -0,0 +1,24 @@
+let url = "https://www.sadikturan.com/";
+let kursAdi = "Komple Web Geliştirme Kursu"
+
+karakter = url.length
+
+console.log(karakter)
+
+kelime = kursAdi.split(" ").length
+
+console.log(kelime)
+
+sorgu = url.startsWith("https");
+
+console.log(sorgu)
+
+sorgu2 = kursAdi.includes("Eğitimi")
+
+console.log(sorgu2)
+
+yenikursAdi = kursAdi.replaceAll(" ", "-").toLowerCase()
+
+merge = url + yenikursAdi
+
+console.log(merge);
