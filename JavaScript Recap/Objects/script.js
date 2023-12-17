037db5816dc3a3f6a4d8b27cee8c964f4ef88852
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-17 14:59:26 +0300

    Create script.js

---
+++ JavaScript Recap/Objects/script.js
@@ -0,0 +1,27 @@
+let user = {
+    "ad": "Bedirhan",
+    "soyad": "Sığırcı",
+    "yas": 23,
+    "Adres": {
+        "sehir": "adana",
+        "ilce": "cukurova"
+    },
+    "hobiler " : ["sinema", "spor"]
+}
+let user2 = {
+    "ad": "Ahmet",
+    "soyad": "Sığırcı",
+    "yas": 23,
+    "Adres": {
+        "sehir": "adana",
+        "ilce": "cukurova"
+    },
+    "hobiler " : ["sinema", "spor"]
+}
+
+let sonuc;
+let users = [user , user2]
+
+sonuc = users[0]
+
+console.log(sonuc)
