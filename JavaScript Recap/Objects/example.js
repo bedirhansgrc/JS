8f2b0aa5f76d467646b3b8625479bfbba2265722
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-20 15:01:05 +0300

    Update example.js

--- JavaScript Recap/Objects/example.js
+++ JavaScript Recap/Objects/example.js
@@ -1,40 +1,40 @@
 let siparis_1 = {
-    "siparis_id" : 101,
-    "siparis_tarihi" : "30.12.2022",
-    "odeme_sekli" : "kredi karti",
-    "kargo_adresi" : {
+    "siparis_id": 101,
+    "siparis_tarihi": "30.12.2022",
+    "odeme_sekli": "kredi karti",
+    "kargo_adresi": {
         "mahalle": "yayha kaptan",
-        "ilce" : "izmit",
-        "sehir" : "kocaeli"
+        "ilce": "izmit",
+        "sehir": "kocaeli"
     },
-    "urunler" : [
-         {  
-            "urun_id" : 5,
+    "urunler": [
+        {
+            "urun_id": 5,
             "urun_adi": "iphone 13 pro",
-            "urun_fiyat" : 22000
+            "urun_fiyat": 22000
         },
         {
-            "urun_id" : 6,
+            "urun_id": 6,
             "urun_adi": "iphone 13 pro max",
-            "urun_fiyat" : 25000
+            "urun_fiyat": 25000
         }
     ]
 }
 
 let siparis_2 = {
-    "siparis_id" : 102,
-    "siparis_tarihi" : "30.12.2022",
-    "odeme_sekli" : "kredi karti",
-    "kargo_adresi" : {
+    "siparis_id": 102,
+    "siparis_tarihi": "30.12.2022",
+    "odeme_sekli": "kredi karti",
+    "kargo_adresi": {
         "mahalle": "yayha kaptan",
-        "ilce" : "izmit",
-        "sehir" : "kocaeli"
+        "ilce": "izmit",
+        "sehir": "kocaeli"
     },
-    "urunler" : [
+    "urunler": [
         {
-            "urun_id" : 6,
+            "urun_id": 6,
             "urun_adi": "iphone 13 pro max",
-            "urun_fiyat" : 25000
+            "urun_fiyat": 25000
         }
     ]
 }
