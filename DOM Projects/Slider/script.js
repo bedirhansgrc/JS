ce003b655fb63bed4f8dbd78da078db5a8849157
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-30 13:32:25 +0300

    updated

--- DOM Projects/Slider/script.js
+++ DOM Projects/Slider/script.js
@@ -21,8 +21,41 @@ var models = [
     }
 
 ]
-var index = 3
-document.querySelector(".card-title").textContent = models[index].name
-document.querySelector(".card-link").setAttribute('href', models[index].link)
-document.querySelector(".card-img-top").setAttribute('src', models[index].image)
+var index = 0
+var slaytCount = models.length
+
+showSlide(index)
+
+document.querySelector(".fa-arrow-left").addEventListener('click', function () {
+
+    index--;
+    showSlide(index)
+    console.log(index)
+})
+document.querySelector(".fa-arrow-right").addEventListener('click', function () {
+
+    index++;
+    showSlide(index)
+    console.log(index)
+})
+
+function showSlide(i){
+
+    index = i
+
+    if(i<0){
+        index = slaytCount -1
+    }
+
+    if(i>=slaytCount){
+        index = 0
+    }
+
+
+    document.querySelector(".card-title").textContent = models[index].name
+    document.querySelector(".card-link").setAttribute('href', models[index].link)
+    document.querySelector(".card-img-top").setAttribute('src', models[index].image)
+    
+
+}
 
