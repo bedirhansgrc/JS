c525a206f63374adaead08a18ebb3bd642769885
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-30 13:16:27 +0300

    updated

--- DOM Projects/Slider/script.js
+++ DOM Projects/Slider/script.js
@@ -1,7 +1,28 @@
 var models = [
-{
-    name : 'Mercedes C200',
-    image : 'pics/MERCEDESC200.JPG'
-}
+    {
+        name: 'Mercedes C200',
+        image: 'pics/MERCEDESC200.jpg',
+        link: 'https://www.mercedes-benz.com.tr/?gagcmid=GA_20739445150_155538493575_679161451432&gad_source=1&gclid=CjwKCAiAnL-sBhBnEiwAJRGiguNxadkqC_3TtvtGI5o84Z7QooTKoGUbPmrjeQsbUfHTiZAyokz98hoCsi8QAvD_BwE&gclsrc=aw.ds&group=all&subgroup=all.saloon&view=BODYTYPE'
+    },
+    {
+        name: 'Toyota Supra',
+        image: 'pics/Supra.jpg',
+        link: 'https://www.toyota.com.tr/'
+    },
+    {
+        name: 'Lamborghini Urus',
+        image: 'pics/Urus.jpg',
+        link: 'https://www.lamborghini.com/en-en'
+    },
+    {
+        name: 'Audi R8',
+        image: 'pics/r8.jpg',
+        link: 'https://www.audi.com.tr/tr/web/tr.html'
+    }
 
 ]
+var index = 3
+document.querySelector(".card-title").textContent = models[index].name
+document.querySelector(".card-link").setAttribute('href', models[index].link)
+document.querySelector(".card-img-top").setAttribute('src', models[index].image)
+
