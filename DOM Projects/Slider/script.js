79f8de015361a5f49c1ed3fcbe7f1bc1c9608cfc
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-30 14:08:46 +0300

    Update script.js

--- DOM Projects/Slider/script.js
+++ DOM Projects/Slider/script.js
@@ -23,6 +23,15 @@ var models = [
 ]
 var index = 0
 var slaytCount = models.length
+var interval
+var settings = {
+    duration: '1000',
+    random: true
+}
+init(settings)
+
+
+
 
 showSlide(index)
 
@@ -39,15 +48,54 @@ document.querySelector(".fa-arrow-right").addEventListener('click', function () 
     console.log(index)
 })
 
-function showSlide(i){
+document.querySelectorAll('.fa-2x').forEach(function(item){
+    item.addEventListener('mouseenter',function(){
+        clearInterval(interval)
+    })
+})
+
+document.querySelectorAll('.fa-2x').forEach(function(item){
+    item.addEventListener('mouseleave',function(){
+        init(settings)
+    })
+})
+
+function init(settings) {
+    var prev
+   interval = setInterval(function () {
+
+        if (settings.random) {
+            //random index
+            do {
+                index = Math.floor(Math.random() * slaytCount)
+            } while (index == prev)
+            prev = index
+        } else {
+            //artan index
+            if (slaytCount == index + 1) {
+                index = -1
+            }
+            showSlide(index)
+            console.log(index)
+            index++
+        }
+        showSlide(index)
+
+    }, settings.duration)
+
+
+
+}
+
+function showSlide(i) {
 
     index = i
 
-    if(i<0){
-        index = slaytCount -1
+    if (i < 0) {
+        index = slaytCount - 1
     }
 
-    if(i>=slaytCount){
+    if (i >= slaytCount) {
         index = 0
     }
 
@@ -55,7 +103,7 @@ function showSlide(i){
     document.querySelector(".card-title").textContent = models[index].name
     document.querySelector(".card-link").setAttribute('href', models[index].link)
     document.querySelector(".card-img-top").setAttribute('src', models[index].image)
-    
+
 
 }
 
