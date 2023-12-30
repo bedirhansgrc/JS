a324b17e4de83aef4ee828848e5940df425252c2
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-30 16:46:14 +0300

    update

--- DOM Projects/Form Validator/script.js
+++ DOM Projects/Form Validator/script.js
@@ -4,49 +4,41 @@ const email = document.getElementById("email")
 const password = document.getElementById("password")
 const repassword = document.getElementById("repassword")
 
-function error(input,message) {
+function error(input, message) {
     input.className = 'form-control is-invalid'
     const div = input.nextElementSibling
     div.innerText = message
-    div.className= 'invalid-feedback'
+    div.className = 'invalid-feedback'
 }
 
 function success(input) {
     input.className = 'form-control is-valid'
 }
 
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if(re.test(input.value)){
+        success(input)
+    }else{
+        error(input, 'Hatalı Bir E-Mail adresi')
+    }
+
+
+}
+
+function checkRequired(inputs){
+    inputs.forEach(function(input){
+        if(input.value === ''){
+            error(input, `${input.id} is required`)
+        }else{
+            success(input)
+        }
+    })
+    }
 
 form.addEventListener("submit", function (e) {
     e.preventDefault()
 
-    if (username.value === '') {
-        error(username, "Username Gerekli")
-    }else{
-        success(username)
-    }
-    if (email.value === '') {
-        error(email, "E-Mail Gerekli")
-    }else if(!validateEmail(email.value)){
-        error(email, "Geçerli bir mail adresi giriniz")
-    }
-    else{
-        success(email)
-    }
-    if (password.value === '') {
-        error(password, "Password Gerekli")
-    }else{
-        success(password)
-    }
-    if (repassword.value === '') {
-        error(repassword, "Repassword Gerekli")
-    }else{
-        success(repassword)
-    }
+    checkRequired([username,email,password,repassword])
+    checkEmail(email)
 })
