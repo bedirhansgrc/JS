2ab0c9b7a39ba7af4b117b5c2b55839aec552f6a
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2023-12-31 16:26:56 +0300

    update

--- DOM Projects/Movie Ticket/script.js
+++ DOM Projects/Movie Ticket/script.js
@@ -1,12 +1,22 @@
 const container = document.querySelector('.container')
-
+const amount = document.querySelector("#amount")
+const count = document.querySelector("#count")
+const select = document.getElementById("movie")
 container.addEventListener('click', function (e) {
     if (e.target.classList.contains("seat") && !e.target.classList.contains("reserved")) {
         e.target.classList.toggle("selected")
-
-        let selectedSeatCount = container.querySelectorAll(".seat.selected").length
-        console.log(selectedSeatCount)
-
-
+        calculateTotal()
     }
 })
+
+select.addEventListener("change", function (e) {
+    calculateTotal()
+})
+
+function calculateTotal(){
+    let selectedSeatCount = container.querySelectorAll(".seat.selected").length
+        let price = select.value
+        count.innerText = selectedSeatCount
+        amount.innerText = selectedSeatCount * price
+
+}
