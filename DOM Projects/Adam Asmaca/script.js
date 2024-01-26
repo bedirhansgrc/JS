5f632052a7cc36ae49360b26f069e2dfe724b63f
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-01-26 17:27:23 +0300

    update

--- DOM Projects/Adam Asmaca/script.js
+++ DOM Projects/Adam Asmaca/script.js
@@ -1,35 +1,50 @@
 const word_el = document.getElementById('word')
 const popup = document.getElementById('popup-container')
 const message_el = document.getElementById("success-message")
-const correctLetters =['j','a','v']
+const correctLetters = []
 const wrongLetters = []
+const selectedWord = getRandomWord()
 
-function getRandomWord(){
+function getRandomWord() {
     const words = ["javascript", "java", "phyton"]
 
     return words[Math.floor(Math.random() * words.length)]
 }
 
 
-function displayWord(){
-    const selectedWord = getRandomWord()
+function displayWord() {
 
     word_el.innerHTML = `
     ${selectedWord.split('').map(letter => `
         <div class= "letter">
-            ${correctLetters.includes(letter) ? letter: ''}
+            ${correctLetters.includes(letter) ? letter : ''}
         </div>
     `).join('')}
     `;
 
-    const w =word_el.innerText.replace(/\n/g, '')
-    if(w == selectedWord){
+    const w = word_el.innerText.replace(/\n/g, '')
+    if (w == selectedWord) {
         popup.style.display = 'flex'
         message_el.innerText = "Tebrikler Kazandıız"
     }
 }
-window.addEventListener("keydown" , function(e){
-    console.log(e.key)
-    console.log(e.keycode)
+window.addEventListener("keydown", function (e) {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter)
+                displayWord()
+            } else {
+                console.log("bu harfi zaten eklediniz")
+            }
+        } else {
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter)
+                console.log("hatalı harfleri güncelle.") 
+            }
+        }
+    }
 })
 displayWord()
