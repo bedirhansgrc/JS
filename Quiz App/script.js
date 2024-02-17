5107698fe3cb0ef8b29765549ddcd16e87343e05
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-02-17 12:22:01 +0300

    Update

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -34,20 +34,37 @@ function Soru(soruMetni, cevapSecenekleri, dogrucevap) {
     this.soruMetni = soruMetni
     this.cevapSecenekleri = cevapSecenekleri
     this.dogrucevap = dogrucevap
-    console.log(this)
 }
-Soru.prototype.cevabiKontrolEt = function(cevap){
+Soru.prototype.cevabiKontrolEt = function (cevap) {
     return cevap === this.dogrucevap
 }
 let soru1 = new Soru("Hangisi javascript paket yönetim uygulamasıdır ?", { a: "Node.js", b: "TypeScript", c: "Npm" }, "c")
-let soru2 = new Soru("Hangisi .net paket yönetim uygulamasıdır ?", {a: "Node.js",b: "nuget",c: "Npm"}, "b")
+let soru2 = new Soru("Hangisi .net paket yönetim uygulamasıdır ?", { a: "Node.js", b: "nuget", c: "Npm" }, "b")
 
 let sorular = [
-    new Soru("Hangisi js paket yönetim uygulamasıdır ?", { a: "Node.js", b: "TypeScript", c: "Npm" }, "c"),
-    new Soru("Hangisi javascript paket yönetim uygulamasıdır ?", { a: "Node.js", b: "TypeScript", c: "Npm" }, "c"),
-    new Soru("Hangisi javascript paket yönetim uygulamasıdır ?", { a: "Node.js", b: "TypeScript", c: "Npm" }, "c"),
-    new Soru("Hangisi javascript paket yönetim uygulamasıdır ?", { a: "Node.js", b: "TypeScript", c: "Npm" }, "c"),
-    new Soru("Hangisi javascript paket yönetim uygulamasıdır ?", { a: "Node.js", b: "TypeScript", c: "Npm" }, "c")
+    new Soru("1-Hangisi js paket yönetim uygulamasıdır ?", { a: "Node.js", b: "TypeScript", c: "Npm" }, "c"),
+    new Soru("2-Hangisi javascript paket yönetim uygulamasıdır ?", { a: "Node.js", b: "TypeScript", c: "Npm" }, "c"),
+    new Soru("3-Hangisi javascript paket yönetim uygulamasıdır ?", { a: "Node.js", b: "TypeScript", c: "Npm" }, "c"),
+    new Soru("4-Hangisi javascript paket yönetim uygulamasıdır ?", { a: "Node.js", b: "TypeScript", c: "Npm" }, "c"),
 ]
 
-console.log(soru1.cevabiKontrolEt("c"))
+function Quiz(sorular) {
+    this.sorular = sorular
+    this.soruIndex = 0
+}
+
+Quiz.prototype.soruGetir = function () {
+    return this.sorular[this.soruIndex]
+}
+
+const quiz = new Quiz(sorular)
+
+
+document.querySelector(".btn-start").addEventListener("click", function(){
+    if (quiz.sorular.length != quiz.soruIndex){
+        console.log(quiz.soruGetir())
+        quiz.soruIndex += 1
+    } else {
+        console.log("quiz bitti")
+    }
+})
