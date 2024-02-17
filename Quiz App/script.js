0519f776ee0f1e2d30949a1598690d247a27e8d1
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-02-17 16:17:19 +0300

    update

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -17,7 +17,20 @@ ui.btn_next.addEventListener("click", function () {
         ui.btn_next.classList.remove("show");
     } else {
         console.log("quiz bitti")
+        ui.score_box.classList.add("active")
+        ui.quiz_box.classList.remove("active")
+        ui.skoruGoster(quiz.sorular.length, quiz.dogruCevapSayisi)
     }
+})
+
+ui.btn_quit.addEventListener("click", function(){
+    window.location.reload()
+})
+ui.btn_replay.addEventListener("click", function(){
+    quiz.soruIndex = 0
+    quiz.dogruCevapSayisi = 0
+    ui.btn_start.click()
+    ui.score_box.classList.remove("active")
 })
 
 function optionSelected(option) {
@@ -25,6 +38,7 @@ function optionSelected(option) {
     let soru = quiz.soruGetir()
 
     if (soru.cevabiKontrolEt(cevap)) {
+        quiz.dogruCevapSayisi += 1
         option.classList.add("correct")
         option.insertAdjacentHTML("beforeend", ui.correctIcon)
     } else {
