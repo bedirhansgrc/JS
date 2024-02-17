c906a61502f3eba9b90ffc0ab0f40744718cbc2a
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-02-17 17:33:22 +0300

    End

--- Quiz App/script.js
+++ Quiz App/script.js
@@ -4,6 +4,7 @@ const ui = new UI()
 ui.btn_start.addEventListener("click", function () {
     ui.quiz_box.classList.add("active")
     startTimer(10)
+    startTimerLine()
     ui.soruGoster(quiz.soruGetir())
     ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length)
     ui.btn_next.classList.remove("show");
@@ -14,12 +15,15 @@ ui.btn_next.addEventListener("click", function () {
         document.querySelector(".quiz_box").classList.add("active")
         quiz.soruIndex += 1
         clearInterval(counter)
+        clearInterval(counterLine)
         startTimer(10)
+        startTimerLine()
         ui.soruGoster(quiz.soruGetir())
         ui.soruSayisiniGoster(quiz.soruIndex + 1, quiz.sorular.length)
         ui.btn_next.classList.remove("show");
     } else {
         clearInterval(counter)
+        clearInterval(counterLine)
         ui.score_box.classList.add("active")
         ui.quiz_box.classList.remove("active")
         ui.skoruGoster(quiz.sorular.length, quiz.dogruCevapSayisi)
@@ -38,6 +42,7 @@ ui.btn_replay.addEventListener("click", function () {
 
 function optionSelected(option) {
     clearInterval(counter)
+    clearInterval(counterLine)
     let cevap = option.querySelector("span b").textContent
     let soru = quiz.soruGetir()
 
@@ -58,26 +63,39 @@ function optionSelected(option) {
 }
 let counter
 function startTimer(time) {
-   counter = setInterval(timer, 1000)
+    counter = setInterval(timer, 1000)
 
-    function timer(){
+    function timer() {
         ui.time_second.textContent = time
         time--
-        if(time<0){
+        if (time < 0) {
             clearInterval(counter)
             ui.time_text.textContent = "Süre Bitti"
 
-           let cevap = quiz.soruGetir().dogrucevap
-           for(let option of ui.option_list.children){
-                if(option.querySelector("span b").textContent == cevap){
+            let cevap = quiz.soruGetir().dogrucevap
+            for (let option of ui.option_list.children) {
+                if (option.querySelector("span b").textContent == cevap) {
                     option.classList.add("correct")
                     option.insertAdjacentHTML("beforeend", ui.correctIcon)
                 }
 
                 option.classList.add("disabled")
-           }
-           ui.btn_next.classList.add("show")
+            }
+            ui.btn_next.classList.add("show")
         }
     }
 }
+let counterLine
+function startTimerLine() {
+    let line_width = 0
 
+    counterLine = setInterval(timer, 10)
+
+    function timer() {
+        line_width += 0.5
+        ui.time_line.style.width = line_width + "px"
+        if (line_width > 549) {
+            clearInterval(counterLine)
+        }
+    }
+}
--- Quiz App/ui.js
+++ Quiz App/ui.js
@@ -9,7 +9,8 @@ function UI() {
         this.falseIcon = `<div class="icon"><i class="fas fa-times"></i></div>`,
         this.score_box = document.querySelector(".score_box"),
         this.time_text = document.querySelector(".time_text"),
-        this.time_second = document.querySelector(".time_second")
+        this.time_second = document.querySelector(".time_second"),
+        this.time_line = document.querySelector(".time_line")
 }
 
 UI.prototype.soruGoster = function (soru) {
