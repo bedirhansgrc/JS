59803f36401d7bcbcdd29beb4d2fbd8f1520ed3c
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-02-19 18:39:28 +0300

    update

--- Modern JS ES6&ES7+/Arrays/script.js
+++ Modern JS ES6&ES7+/Arrays/script.js
@@ -1,15 +1,15 @@
 const sayilar = [1, 5, 7, 3, 2, 67]
+const urunler = [
+    {urunAdi: "iphone 11",fiyat: 12000},
+    {urunAdi: "iphone 12",fiyat: 15000},
+    {urunAdi: "iphone 13",fiyat: 17000},
+    {urunAdi: "iphone 14",fiyat: 19000}
+]
 
-// const tekSayilar = []
-// for (let sayi of sayilar) {
-//     if (sayi % 2 == 1) {
-//         tekSayilar.push(sayi)
-//     }
-// }
+//const tekSayilar = sayilar.filter((sayi) => sayi % 2 == 1)
+const tekSayilar = sayilar.map((sayi) => sayi % 2 == 1)
+const sonuc = urunler.filter(urun => urun.fiyat > 15000).map(urun => urun.urunAdi)
 
 
-const tekSayiKontrol = (sayi) => sayi % 2 == 1
 
-const tekSayilar = sayilar.filter(tekSayiKontrol)
-
-console.log(tekSayilar)
+console.log(sonuc)
