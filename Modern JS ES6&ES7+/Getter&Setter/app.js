1e2e61f2e964cf19ee8ce733e6864a7d9ae1ed59
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-02-21 18:25:37 +0300

    Create app.js

---
+++ Modern JS ES6&ES7+/Getter&Setter/app.js
@@ -0,0 +1,40 @@
+class kisi {
+    constructor(ad, meslek, dogumYili) {
+        this.ad = ad
+        this.meslek = meslek
+        this.dogumYili = dogumYili
+    }
+
+    yasHesapla() {
+        let tarih = new Date().getFullYear()
+        return tarih - this.dogumYili
+    }
+
+    get ad() {
+        return this._ad
+    }
+
+    set ad(value) {
+        if (value.length < 3) {
+            console.log("ad için çok az karakter")
+            return
+        }
+        this._ad = value
+    }
+
+    get dogumYili() {
+        return this._dogumYili
+    }
+
+    set dogumYili(value) {
+        if (value < 1900 || value > new Date().getFullYear()) {
+            console.log("tarih bilgisi yanlış")
+            return
+        }
+        this._dogumYili =value
+    }
+}
+
+let kisi1 = new kisi("çınar", "ogrenci", 2012)
+
+console.log(kisi1.ad)
