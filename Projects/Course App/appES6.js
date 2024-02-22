3e310f6fd9e8c6b327a91ea76a783717ab5b4fba
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-02-22 15:53:21 +0300

    es5 transformed to es6+

---
+++ Projects/Course App/appES6.js
@@ -0,0 +1,83 @@
+class Course {
+    constructor(title, instructor, image) {
+        this.title = title
+        this.instructor = instructor
+        this.image = image
+    }
+}
+
+class UI {
+    addCourseToList(course) {
+        const list = document.getElementById("course-list")
+
+        var html = `
+        <tr>
+            <td><img style="height: 100px; width: 200px;" src = "img/${course.image}"/></td>
+            <td>${course.title}</td>
+            <td>${course.instructor}</td>
+            <td><a href ="#" class ="btn btn-danger btn-sm delete">Delete</a></td>
+
+        </tr>
+    
+    `
+
+        list.innerHTML += html
+    }
+    clearControls() {
+        const title = document.getElementById("title").value = ""
+        const instructor = document.getElementById("instructor").value = ""
+        const image = document.getElementById("image").value = ""
+    }
+    deleteCourse(element) {
+        if (element.classList.contains("delete")) {
+            element.parentElement.parentElement.remove()
+        }
+    }
+    showAlert(message, className) {
+        var alert = `
+        <div class= "alert alert-${className}">
+            ${message}
+        </div>
+    `
+        const row = document.querySelector(".row")
+        row.insertAdjacentHTML('beforeBegin', alert)
+
+        setTimeout(() => {
+            document.querySelector(".alert").remove()
+        }, 2000)
+
+    }
+
+}
+document.getElementById("new-course").addEventListener("submit", function (e) {
+
+    const title = document.getElementById("title").value
+    const instructor = document.getElementById("instructor").value
+    const image = document.getElementById("image").value
+
+    //create course object
+    const course = new Course(title, instructor, image)
+
+    //Crete UI
+    const ui = new UI()
+
+    if (title === '' || instructor === '' || image === '') {
+        ui.showAlert("Please complete the form", 'warning')
+    } else {
+        //add course to list
+        ui.addCourseToList(course)
+
+        //clear controls
+        ui.clearControls()
+
+        ui.showAlert("The course has been added", 'success')
+    }
+
+    e.preventDefault()
+})
+
+document.getElementById("course-list").addEventListener("click", function (e) {
+    const ui = new UI()
+    ui.deleteCourse(e.target)
+    ui.showAlert('The course has been deleted', 'danger')
+})
