bb32782aa41d02e8c0efb5d812642f74adc6cd1b
Bedirhan Sığırcı <bedirhansigirci@gmail.com>
2024-02-22 14:35:04 +0300

    update

--- Projects/Course App/appES5.js
+++ Projects/Course App/appES5.js
@@ -1,25 +1,54 @@
-function Course(title,instructor,image){
+//Course Constructor
+function Course(title, instructor, image) {
     this.title = title
     this.instructor = instructor
     this.image = image
 }
+//UI Constructor
+function UI() {
+}
 
+UI.prototype.addCourseToList = function (course) {
+    const list = document.getElementById("course-list")
 
+    var html = `
+        <tr>
+            <td><img src = "img/${course.image}"/</td>
+            <td>${course.title}</td>
+            <td>${course.instructor}</td>
+            <td><a href ="#" class ="btn btn-danger btn-sm">Delete</a></td>
 
-document.getElementById("new-course").addEventListener("submit",function(e){
+        </tr>
+    
+    `
+
+    list.innerHTML += html
+}
+
+UI.prototype.clearControls = function () {
+    const title = document.getElementById("title").value = ""
+    const instructor = document.getElementById("instructor").value = ""
+    const image = document.getElementById("image").value = ""
+
+}
+
+document.getElementById("new-course").addEventListener("submit", function (e) {
 
     const title = document.getElementById("title").value
     const instructor = document.getElementById("instructor").value
     const image = document.getElementById("image").value
 
     //create course object
-    const course = new Course(title,instructor,image)
+    const course = new Course(title, instructor, image)
 
+    //Crete UI
+    const ui = new UI()
 
-    console.log(course)
-    //save to database
+    //add course to list
+    ui.addCourseToList(course)
 
-    //show on the UI
+    //clear controls
+    ui.clearControls()
 
     e.preventDefault()
 })
